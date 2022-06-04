699e12f1b7981aef567b36f13ae9afa8f525764c
Lalluviadel <inspiracion@yandex.ru>
2022-06-04 23:59:24 +0300

    - added docker files; - collectstatic done;

--- Tiny_shop/settings/debug.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from .base import *
-
-SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = True
-SITE_ID = 1
--- Tiny_shop/settings/prod.py
+++
@@ -1,7 +0,0 @@
-import os
-
-from .base import *
-
-SECRET_KEY = os.environ['SECRET_KEY']
-DEBUG = False
-ALLOWED_HOSTS = ['*']
